791de7e6b77c35f0eb4f12c23cb76640e183b080
proximaCent <sabikaftahee01@gmail.com>
2024-06-27 15:03:43 +0600

    Added a semaphore to ensure that adding or removing a customer is thread-safe

--- GroceryQueue.java
+++ GroceryQueue.java
@@ -1,28 +1,55 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.Semaphore;
 
 public class GroceryQueue {
     private final int maxLength;
     private final Queue<Customer> queue;
+    private final Semaphore queueSemaphore;
 
     public GroceryQueue(int maxLength) {
         this.maxLength = maxLength;
         this.queue = new LinkedList<>();
+        this.queueSemaphore = new Semaphore(1);
     }
 
     public boolean addCustomer(Customer customer) {
-        if (queue.size() < maxLength) {
-            queue.offer(customer);
-            return true;
+        try {
+            queueSemaphore.acquire();
+            if (queue.size() < maxLength) {
+                queue.offer(customer);
+                return true;
+            }
+            return false;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return false;
+        } finally {
+            queueSemaphore.release();
         }
-        return false;
     }
 
     public Customer getNextCustomer() {
-        return queue.poll();
+        try {
+            queueSemaphore.acquire();
+            return queue.poll();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        } finally {
+            queueSemaphore.release();
+        }
     }
 
     public int getSize() {
-        return queue.size();
+        try {
+            queueSemaphore.acquire();
+            return queue.size();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return -1;
+        } finally {
+            queueSemaphore.release();
+        }
     }
 }
