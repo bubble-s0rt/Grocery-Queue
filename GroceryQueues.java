29b195f4ca0b5a5d5196ba6ba81656dd46e3b3cc
proximaCent <sabikaftahee01@gmail.com>
2024-06-27 15:05:28 +0600

    added semaphor to make it threadd safe while adding cunstome

--- GroceryQueues.java
+++ GroceryQueues.java
@@ -1,27 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class GroceryQueues {
     private final GroceryQueue[] queues;
     private final Random random;
+    private final Semaphore[] queueSemaphores;
 
     public GroceryQueues(int numberOfQueues, int maxLength) {
         queues = new GroceryQueue[numberOfQueues];
+        queueSemaphores = new Semaphore[numberOfQueues];
         for (int i = 0; i < numberOfQueues; i++) {
             queues[i] = new GroceryQueue(maxLength);
+            queueSemaphores[i] = new Semaphore(1);
         }
         this.random = new Random();
     }
 
     public boolean addCustomer(Customer customer) {
         GroceryQueue shortestQueue = queues[0];
-        for (GroceryQueue queue : queues) {
-            if (queue.getSize() < shortestQueue.getSize()) {
-                shortestQueue = queue;
+        Semaphore shortestQueueSemaphore = queueSemaphores[0];
+        
+        for (int i = 1; i < queues.length; i++) {
+            if (queues[i].getSize() < shortestQueue.getSize()) {
+                shortestQueue = queues[i];
+                shortestQueueSemaphore = queueSemaphores[i];
             }
         }
-        return shortestQueue.addCustomer(customer);
+        
+        try {
+            shortestQueueSemaphore.acquire();
+            return shortestQueue.addCustomer(customer);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return false;
+        } finally {
+            shortestQueueSemaphore.release();
+        }
     }
 
     public GroceryQueue[] getQueues() {
